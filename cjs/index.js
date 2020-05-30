a2a65506fa7956bee2b0db1c63544571cd18d175
dorilama <assioma.xi@gmail.com>
2020-05-30 18:28:03 +0100

    add dev dependencies and build

--- cjs/index.js
+++ cjs/index.js
@@ -3,8 +3,60 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.default = void 0;
+exports.connectStore = void 0;
 
-var _default = () => {};
+const connectStore = (store, {
+  tag = "store-provider",
+  compare = (a, b) => a === b
+}) => {
+  customElements.whenDefined("uce-lib").then(() => {
+    const {
+      define
+    } = customElements.get("uce-lib");
+    define(tag, {
+      provide(observedProps = Object.keys(store.state)) {
+        this.observedProps = observedProps;
+        this.unsubscribeFromStore = store.on("set", (store, prop, newVal, oldVal) => {
+          if (this.observedProps.includes(prop) && !compare(oldVal, newVal)) {
+            this.render();
+          }
+        });
+        observedProps.forEach(prop => {
+          Object.defineProperty(this, prop, {
+            get() {
+              return store.state[prop];
+            },
 
-exports.default = _default;
+            set(val) {
+              throw `You need to commit an action to change the value of ${prop} in the state. E.g. this.commit('mutationName',args)`;
+            }
+
+          });
+        });
+      },
+
+      disconnected() {
+        this.unsubscribeFromStore();
+      },
+
+      commit(mutationName, ...mutationArgs) {
+        store.commit(mutationName, ...mutationArgs);
+      },
+
+      dispatch(actionName, ...actionArgs) {
+        store.dispatch(actionName, ...actionArgs);
+      },
+
+      get getters() {
+        return store.getters;
+      },
+
+      set getters(val) {
+        throw `Getters can't be set. Define them in the store.`;
+      }
+
+    });
+  });
+};
+
+exports.connectStore = connectStore;
